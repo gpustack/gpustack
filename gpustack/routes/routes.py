72105035ea45dacd5ffd58979dbc1ab8d368612e
gitlawr <lawrleegle@gmail.com>
2024-07-11 17:07:57 +0800

    feat: dynamic log level

--- gpustack/routes/routes.py
+++ gpustack/routes/routes.py
@@ -4,6 +4,7 @@ from gpustack.routes import (
     api_keys,
     auth,
     dashboard,
+    debug,
     gpu_devices,
     model_instances,
     probes,
@@ -23,28 +24,35 @@ api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 
 # authed routes
 
-base_router = APIRouter(dependencies=[Depends(get_current_user)])
-base_router.include_router(users.me_router, prefix="/users", tags=["Users"])
-base_router.include_router(api_keys.router, prefix="/api-keys", tags=["API Keys"])
+v1_base_router = APIRouter(dependencies=[Depends(get_current_user)])
+v1_base_router.include_router(users.me_router, prefix="/users", tags=["Users"])
+v1_base_router.include_router(api_keys.router, prefix="/api-keys", tags=["API Keys"])
 
-admin_router = APIRouter()
-admin_router.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
-admin_router.include_router(users.router, prefix="/users", tags=["Users"])
-admin_router.include_router(models.router, prefix="/models", tags=["Models"])
-admin_router.include_router(workers.router, prefix="/workers", tags=["Workers"])
-admin_router.include_router(
+v1_admin_router = APIRouter()
+v1_admin_router.include_router(
+    dashboard.router, prefix="/dashboard", tags=["Dashboard"]
+)
+v1_admin_router.include_router(users.router, prefix="/users", tags=["Users"])
+v1_admin_router.include_router(models.router, prefix="/models", tags=["Models"])
+v1_admin_router.include_router(workers.router, prefix="/workers", tags=["Workers"])
+v1_admin_router.include_router(
     model_instances.router, prefix="/model-instances", tags=["Model Instances"]
 )
-admin_router.include_router(
+v1_admin_router.include_router(
     gpu_devices.router, prefix="/gpu-devices", tags=["GPU Devices"]
 )
 
-
 api_router.include_router(
-    base_router, dependencies=[Depends(get_current_user)], prefix="/v1"
+    v1_base_router, dependencies=[Depends(get_current_user)], prefix="/v1"
 )
 api_router.include_router(
-    admin_router, dependencies=[Depends(get_admin_user)], prefix="/v1"
+    v1_admin_router, dependencies=[Depends(get_admin_user)], prefix="/v1"
+)
+api_router.include_router(
+    debug.router,
+    dependencies=[Depends(get_admin_user)],
+    prefix="/debug",
+    include_in_schema=False,
 )
 api_router.include_router(
     openai.router,
