56ecf80110928fced86f2ed0171c05c4e4cb83ff
gitlawr <lawrleegle@gmail.com>
2024-07-17 16:32:06 +0800

    fix: install script compatible with sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 set -o noglob
 
@@ -58,7 +58,7 @@ verify_system() {
 # Function to check if the script is run as root or has sudo permissions
 SUDO=
 check_root() {
-  if [ "$EUID" -ne 0 ]; then
+  if [ "$(id -u)" -ne 0 ]; then
     if command -v sudo >/dev/null 2>&1; then
       info "running as non-root, will use sudo for installation."
       SUDO="sudo"
@@ -70,49 +70,68 @@ check_root() {
 
 # Function to detect the OS and package manager
 detect_os() {
-  if [ "$(uname)" == "Darwin" ]; then
+  if [ "$(uname)" = "Darwin" ]; then
     OS="macos"
   elif [ -f /etc/os-release ]; then
     # shellcheck disable=SC1091
     . /etc/os-release
     OS=$ID
   else
-    fatal "Unsupported OS. Only Linux and macOS are supported."
+    fatal "Unsupported OS. Only Linux and MacOS are supported."
   fi
 }
 
 # Function to check and install Python
+PYTHONPATH=""
 check_python() {
-  if ! command -v python3 &> /dev/null; then
+  if ! command -v python3 > /dev/null 2>&1; then
     info "Python3 could not be found. Attempting to install..."
-    if [ "$OS" == "ubuntu" ] || [ "$OS" == "debian" ]; then
+    if [ "$OS" = "ubuntu" ] || [ "$OS" = "debian" ]; then
       $SUDO apt update && apt install -y python3
-    elif [ "$OS" == "centos" ] || [ "$OS" == "rhel" ]; then
+    elif [ "$OS" = "centos" ] || [ "$OS" = "rhel" ]; then
       $SUDO yum install -y python3
-    elif [ "$OS" == "macos" ]; then
+    elif [ "$OS" = "macos" ]; then
       $SUDO brew install python
     else
       fatal "Unsupported OS for automatic Python installation. Please install Python3 manually."
     fi
   fi
-  
+
   PYTHON_VERSION=$(python3 -c "import sys; print(sys.version_info.major * 10 + sys.version_info.minor)")
   if [ "$PYTHON_VERSION" -lt 40 ]; then
     fatal "Python version is less than 3.10. Please upgrade Python to at least version 3.10."
   fi
 
-  if ! command -v pipx &> /dev/null; then
+  if ! command -v pip3 > /dev/null 2>&1; then
+    info "Pip3 could not be found. Attempting to install..."
+    if [ "$OS" = "ubuntu" ] || [ "$OS" = "debian" ]; then
+      $SUDO apt update && $SUDO apt install -y python3-pip
+    elif [ "$OS" = "centos" ] || [ "$OS" = "rhel" ]; then
+      $SUDO yum install -y python3-pip
+    elif [ "$OS" = "macos" ]; then
+      $SUDO brew install python
+    else
+      fatal "Unsupported OS for automatic Pip3 installation. Please install Pip3 manually."
+    fi
+  fi
+
+  PYTHONPATH=$(python3 -c 'import sys; print(":".join(sys.path))')
+  
+  if ! command -v pipx > /dev/null 2>&1; then
     info "Pipx could not be found. Attempting to install..."
     pip3 install pipx
     pipx ensurepath
+
+    PIPX_BIN_DIR=$(pipx environment --value PIPX_BIN_DIR)
+    export PATH="$PIPX_BIN_DIR:$PATH"
   fi
 }
 
 # Function to install dependencies
 install_dependencies() {
-  DEPENDENCIES=("curl" "sudo")
-  for dep in "${DEPENDENCIES[@]}"; do
-    if ! command -v "$dep" &> /dev/null; then
+  DEPENDENCIES="curl sudo"
+  for dep in $DEPENDENCIES; do
+    if ! command -v "$dep" > /dev/null 2>&1; then
       fatal "$dep is required but missing. Please install $dep."
     fi
   done
@@ -120,7 +139,7 @@ install_dependencies() {
 
 # Function to check CUDA for NVIDIA GPUs
 check_cuda() {
-  if command -v nvidia-smi &> /dev/null; then
+  if command -v nvidia-smi > /dev/null 2>&1; then
     if ! dpkg-query -W cuda; then
       fatal "NVIDIA GPU detected but CUDA is not installed. Please install CUDA."
     fi
@@ -137,6 +156,8 @@ Description=GPUStack Service
 [Service]
 ExecStart=$(which gpustack) start $@
 Restart=always
+StandardOutput=append:/var/log/gpustack.log
+StandardError=append:/var/log/gpustack.log
 
 [Install]
 WantedBy=multi-user.target
@@ -171,6 +192,10 @@ EOF
   </array>
   <key>RunAtLoad</key>
   <true/>
+  <key>StandardOutPath</key>
+  <string>/var/log/gpustack.log</string>
+  <key>StandardErrorPath</key>
+  <string>/var/log/gpustack.log</string>
 </dict>
 </plist>
 EOF
@@ -180,7 +205,7 @@ EOF
 
 # Function to setup and start the service
 setup_and_start() {
-  if [ "$OS" == "macos" ]; then
+  if [ "$OS" = "macos" ]; then
     setup_launchd "$@"
   else
     setup_systemd "$@"
@@ -192,9 +217,13 @@ create_uninstall_script() {
   $SUDO mkdir -p /var/lib/gpustack
   $SUDO tee /var/lib/gpustack/uninstall.sh > /dev/null <<EOF
 #!/bin/bash
-pipx uninstall gpustack > /dev/null
+set -e
+export PYTHONPATH="$PYTHONPATH"
+export PIPX_HOME=$(pipx environment --value PIPX_HOME)
+export PIPX_BIN_DIR=$(pipx environment --value PIPX_BIN_DIR)
+$(which pipx) uninstall gpustack > /dev/null
 rm -rf /var/lib/gpustack
-if [ "$OS" == "macos" ]; then
+if [ "$OS" = "macos" ]; then
   launchctl unload /Library/LaunchDaemons/ai.gpustack.plist
   rm /Library/LaunchDaemons/ai.gpustack.plist
 else
@@ -210,22 +239,21 @@ EOF
 
 # Function to install GPUStack using pipx
 install_gpustack() {
-  if command -v gpustack &> /dev/null; then
+  if command -v gpustack > /dev/null 2>&1; then
     info "GPUStack is already installed."
     return
   fi
-  local install_args=()
+
+  install_args=""
   if [ "$INSTALL_PRE_RELEASE" -eq 1 ]; then
-    install_args+=(--pip-args='--pre')
+    # shellcheck disable=SC2089
+    install_args="--pip-args='--pre'"
   fi
 
-  info "Installing GPUStack with $INSTALL_PACKAGE_SPEC..."
+  echo "Installing GPUStack..."
 
-  $SUDO pipx install "${install_args[@]}" "${INSTALL_PACKAGE_SPEC}" --verbose
-  $SUDO pipx ensurepath
-
-  PIPX_BIN_DIR=$($SUDO pipx environment --value PIPX_BIN_DIR)
-  export PATH="$PIPX_BIN_DIR:$PATH"
+  # shellcheck disable=SC2090
+  pipx install $install_args "$INSTALL_PACKAGE_SPEC" --verbose
 }
 
 # Main install process
