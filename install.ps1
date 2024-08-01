5f715f94941cb8e6c9a0b54a440314cf7c93e299
michelia <michelia@seal.io>
2024-08-01 14:41:52 +0800

    refactor: update windows install params

--- install.ps1
+++ install.ps1
@@ -5,181 +5,16 @@
 .DESCRIPTION
     Run GPUStack server or worker with the specified settings.
 
-.PARAMETER ConfigFile
-    Path to the YAML config file.
-    Alias: config-file
-
-.PARAMETER DataDir
-    Common settings. Directory to store data. Default is OS specific.
-    Alias: data-dir
-
-.PARAMETER Token
-    Common settings. Shared secret used to add a worker.
-    Alias: token
-
-.PARAMETER ServerHost
-    Server settings. Host to bind the server to.
-    Alias: server-host
-
-.PARAMETER ServerPort
-    Server settings. Port to bind the server to.
-    Alias: server-port
-
-.PARAMETER DatabaseURL
-    Server settings. URL of the database. Example: postgresql://user:password@hostname:port/db_name.
-    Alias: database-url
-
-.PARAMETER DisableWorker
-    Server settings. Disable embedded worker.
-    Alias: disable-worker
-
-.PARAMETER BootstrapPassword
-    Server settings. Initial password for the default admin user. Random by default.
-    Alias: bootstrap-password
-
-.PARAMETER SystemReservedMemory
-    Server settings. The system reserves resources for the worker during scheduling, measured in GiB.
-    By default, 1 GiB of memory is reserved.
-    Alias: system-reserved-memory
-
-.PARAMETER SystemReservedGPUMemory
-    Server settings. The system reserves resources for the worker during scheduling, measured in GiB.
-    By default, 1 GiB of GPU memory is reserved.
-    Alias: system-reserved-gpu-memory
-
-.PARAMETER SSLKeyFile
-    Server settings. Path to the SSL key file.
-    Alias: ssl-key-file
-
-.PARAMETER SSLCertFile
-    Server settings. Path to the SSL certificate file.
-    Alias: ssl-cert-file
-
-.PARAMETER ForceAuthLocalhost
-    Server settings. Force authentication for requests originating from localhost (127.0.0.1).
-    When set to True, all requests from localhost will require authentication.
-    Alias: force-auth-localhost
-
-
-.PARAMETER ServerURL
-    Worker settings. Server to connect to.
-    Alias: server-url
-
-.PARAMETER WorkerIP
-    Worker settings. IP address of the worker node. Auto-detected by default.
-    Alias: worker-ip
-
-.PARAMETER EnableMetrics
-    Worker settings. IP address of the worker node. Auto-detected by default.
-    Alias: enable-metrics
-
-.PARAMETER MetricsPort
-    Worker settings. Enable metrics.
-    Alias: metrics-port
-
-.PARAMETER WorkerPort
-    Worker settings. Port to expose metrics.
-    Alias: worker-port
-
-.PARAMETER LogDir
-    Worker settings. Directory to store logs.
-    Alias: log-dir
-
 .EXAMPLE
     .\install.ps1
 
     You can start the GPUStack server by running the command on a server node.
 
 .EXAMPLE
-    .\install.ps1 -server-url http://myserver -token mytoken
+    .\install.ps1 --server-url http://myserver --token mytoken
 
     You can add additional workers to form a GPUStack cluster by running the command on worker nodes.
 #>
-
-[CmdletBinding()]
-param (
-    [Alias("config-file")]
-    [Parameter()]
-    [String]$ConfigFile,
-
-    [Alias("data-dir")]
-    [Parameter()]
-    [String]$DataDir,
-
-    [Parameter()]
-    [String]$Token,
-
-    [Alias("server-host")]
-    [Parameter()]
-    [String]$ServerHost = "0.0.0.0",
-
-    [Alias("server-port")]
-    [Parameter()]
-    [String]$ServerPort,
-
-    [Alias("database-url")]
-    [Parameter()]
-    [String]$DatabaseURL,
-
-    [Alias("bootstrap-password")]
-    [Parameter()]
-    [String]$BootstrapPassword,
-
-    [Alias("disable-worker")]
-    [Parameter()]
-    [Switch]$DisableWorker,
-
-    [Alias("system-reserved-memory")]
-    [Parameter()]
-    [int]$SystemReservedMemory = 1,
-
-    [Alias("system-reserved-gpu-memory")]
-    [Parameter()]
-    [int]$SystemReservedGPUMemory = 1,
-
-    [Alias("ssl-key-file")]
-    [Parameter()]
-    [String]$SSLKeyFile,
-
-    [Alias("ssl-cert-file")]
-    [Parameter()]
-    [String]$SSLCertFile,
-
-    [Alias("force-auth-localhost")]
-    [Parameter()]
-    [switch]$ForceAuthLocalhost,
-
-    [Alias("server-url")]
-    [Parameter()]
-    [String]$ServerURL,
-
-    [Alias("worker-ip")]
-    [Parameter()]
-    [String]$WorkerIP,
-
-    [Alias("enable-metrics")]
-    [Parameter()]
-    [Bool]$EnableMetrics = $true,
-
-    [Alias("metric-port")]
-    [Parameter()]
-    [Int]$MetricsPort = 10151,
-
-    [Alias("worker-port")]
-    [Parameter()]
-    [Int]$WorkerPort = 10150,
-
-    [Alias("log-dir")]
-    [Parameter()]
-    [String]$LogDir,
-
-    [Switch]$Help
-)
-
-if ($Help) {
-    Get-Help -Full $MyInvocation.MyCommand.Definition
-    exit
-}
 
 $ErrorActionPreference = "Stop"
 
@@ -235,93 +70,96 @@ function Check-CUDA {
 }
 
 function Get-Arg {
+    param (
+        [Parameter(ValueFromRemainingArguments = $true)]
+        [string[]]$RemainingArgs
+    )
+
     Log-Info "Getting arguments from flags..."
 
     $envList = @()
-    if ($Debug) {
-        $envList += "GPUSTACK_DEBUG=true"
-    }
 
-    if ($ConfigFile) {
-        $envList += "GPUSTACK_CONFIG_File=$ConfigFile"
-    }
-
-    if ($DataDir) {
-        $envList += "GPUSTACK_DATA_DIR=$DataDir"
-    }
-
-    if ($Token) {
-        $envList += "GPUSTACK_TOKEN=$Token"
-    }
-
-    if ($ServerHost) {
-        $envList += "GPUSTACK_HOST=$ServerHOST"
-    }
-
-    if ($ServerPort) {
-        $envList += "GPUSTACK_PORT=$ServerPort"
-    }
-
-    if ($DatabaseURL) {
-        $envList += "GPUSTACK_DATABASE_URL=$DatabaseURL"
-    }
-
-    if ($BootstrapPassword) {
-        $envList += "GPUSTACK_BOOTSTRAP_PASSWORD=$BootstrapPassword"
-    }
-
-    if ($DisableWorker) {
-        $envList += "GPUSTACK_DISABLE_WORKER=true"
-    }
-
-    if ($SystemReservedMemory -or $SystemReservedGPUMemory) {
-        $reserved = @{
-            memory     = $SystemReservedMemory
-            gpu_memory = $SystemReservedGPUMemory
+    for ($i = 0; $i -lt $RemainingArgs.Count; $i++) {
+        $value = $RemainingArgs[$i + 1]
+        switch ($RemainingArgs[$i]) {
+            "--debug" {
+                $envList += "GPUSTACK_DEBUG=$value"
+                $i++
+            }
+            "--config-file" {
+                $envList += "GPUSTACK_CONFIG_File=$value"
+                $i++
+            }
+            "--data-dir" {
+                $envList += "GPUSTACK_DATA_DIR=$value"
+                $i++
+            }
+            "--token" {
+                $envList += "GPUSTACK_TOKEN=$value"
+                $i++
+            }
+            "--host" {
+                $envList += "GPUSTACK_HOST=$value"
+                $i++
+            }
+            "--port" {
+                $envList += "GPUSTACK_PORT=$value"
+                $i++
+            }
+            "--database-url" {
+                $envList += "GPUSTACK_DATABASE_URL=$value"
+                $i++
+            }
+            "--bootstrap-password" {
+                $envList += "GPUSTACK_BOOTSTRAP_PASSWORD=$value"
+                $i++
+            }
+            "--disable-worker" {
+                $envList += "GPUSTACK_DISABLE_WORKER=$value"
+                $i++
+            }
+            "--system-reserved" {
+                $escapedJsonString = $value -replace '"', '\`"'
+                $envList += "GPUSTACK_SYSTEM_RESERVED=`"$escapedJsonString`""
+                $i++
+            }
+            "--ssl-keyfile" {
+                $envList += "GPUSTACK_SSL_KEY_FILE=$value"
+                $i++
+            }
+            "--ssl-certfile" {
+                $envList += "GPUSTACK_SSL_CERT_FILE=$value"
+                $i++
+            }
+            "--force-auth-localhost" {
+                $envList += "GPUSTACK_FORCE_AUTH_LOCALHOST=$value"
+                $i++
+            }
+            "--server-url" {
+                $envList += "GPUSTACK_SERVER_URL=$value"
+                $i++
+            }
+            "--worker-ip" {
+                $envList += "GPUSTACK_WORKER_IP=$value"
+                $i++
+            }
+            "--worker-port" {
+                $envList += "GPUSTACK_WORKER_PORT=$value"
+                $i++
+            }
+            "--enable-metrics" {
+                $envList += "GPUSTACK_ENABLE_METRICS=$value"
+                $i++
+            }
+            "--metrics-port" {
+                $envList += "GPUSTACK_METRICS_PORT=$value"
+                $i++
+            }
+            "--log-dir" {
+                $envList += "GPUSTACK_LOG_DIR=$value"
+                $i++
+            }
         }
-
-        $jsonString = $reserved | ConvertTo-Json -Compress
-        $escapedJsonString = $jsonString -replace '"', '\`"'
-        $envList += "GPUSTACK_SYSTEM_RESERVED=`"$escapedJsonString`""
-    }
-
-    if ($SSLKeyFile) {
-        $envList += "GPUSTACK_SSL_KEY_FILE=$SSLKeyFile"
-    }
-
-    if ($SSLCertFile) {
-        $envList += "GPUSTACK_SSL_CERT_FILE=$SSLCertFile"
-    }
-
-    if ($ForceAuthLocalhost) {
-        $envList += "GPUSTACK_FORCE_AUTH_LOCALHOST= true"
-    }
-
-    if ($ServerURL) {
-        $envList += "GPUSTACK_SERVER_URL=$ServerURL"
-    }
-
-    if ($WorkerIP) {
-        $envList += "GPUSTACK_WORKER_IP=$WorkerIP"
-    }
-
-    if ($WorkerPort) {
-        $envList += "GPUSTACK_WORKER_PORT=$WorkerPort"
-    }
-
-    if ($EnableMetrics -eq $true -or $EnableMetrics -eq 1) {
-        $envList += "GPUSTACK_ENABLE_METRICS=true"
-    }
-    else {
-        $envList += "GPUSTACK_ENABLE_METRICS=false"
-    }
-
-    if ($MetricsPort) {
-        $envList += "GPUSTACK_METRICS_PORT=$MetricsPort"
-    }
-
-    if ($LogDir) {
-        $envList += "GPUSTACK_LOG_DIR=$LogDir"
     }
 
     $envList += "APPDATA=$env:APPDATA"
@@ -535,6 +373,10 @@ function Install-GPUStack {
 }
 
 function Setup-GPUStackService {
+    param (
+        [string]$envListString
+    )
+
     $serviceName = "GPUStack"
     $serviceDisplayName = "GPUStack"
     $exePath = $(Get-Command gpustack).Source
@@ -582,8 +424,6 @@ function Setup-GPUStackService {
 
         $null = New-Item -Path $gpustackDirectoryPath -ItemType "Directory" -ErrorAction SilentlyContinue -Force
         $null = New-Item -Path $gpustackLogDirectoryPath -ItemType "Directory" -ErrorAction SilentlyContinue -Force
-
-        $envListString = Get-Arg
 
         $null = nssm install $serviceName $exePath
         if ($LASTEXITCODE -ne 0) {
@@ -806,7 +646,8 @@ try {
     Install-NSSM
     Install-GPUStack
     Create-UninstallScript
-    Setup-GPUStackService
+    $envListString = Get-Arg @args
+    Setup-GPUStackService -envListString $envListString
 }
 catch {
     Log-Fatal "Failed to install GPUStack: `"$($_.Exception.Message)`""
