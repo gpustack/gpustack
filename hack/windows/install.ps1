0dd4d915de936061dd52f9055076aa6b877c4d3a
michelia <michelia@seal.io>
2024-08-06 20:23:25 +0800

    chore: upgrade llama-box to 0.0.26

--- hack/windows/install.ps1
+++ hack/windows/install.ps1
@@ -139,7 +139,7 @@ function Get-GGUFParser {
 }
 
 function Get-LlamaBox {
-    $version = "v0.0.24"
+    $version = "v0.0.26"
     $llamaBoxDir = Join-Path -Path $THIRD_PARTY_DIR -ChildPath "llama-box"
     $llamaBoxTmpDir = Join-Path -Path $llamaBoxDir -ChildPath "tmp"
 
