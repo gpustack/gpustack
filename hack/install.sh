bb7f86050282f814368d82d1f636b0191fc69426
michelia <michelia@seal.io>
2024-08-08 12:01:11 +0800

    chore: upgrade gguf-parser to 0.6.2

--- hack/install.sh
+++ hack/install.sh
@@ -86,7 +86,7 @@ function download_ui() {
 }
 
 function download_gguf_parser() {
-    local version="v0.6.1"
+    local version="v0.6.2"
 
     local gguf_parser_dir="${THIRD_PARTY_DIR}/gguf-parser"
     mkdir -p "${gguf_parser_dir}"
