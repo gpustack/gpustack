a4c5f75c21c2a3095f15a965e0aa53156c06514a
gitlawr <lawrleegle@gmail.com>
2024-09-10 15:56:39 +0800

    fix: reserve carriage return characters in logs

--- gpustack/worker/logs.py
+++ gpustack/worker/logs.py
@@ -38,7 +38,10 @@ LogOptionsDep = Annotated[LogOptions, Depends(get_log_options)]
 def log_generator(path: str, options: LogOptions):
     logger.debug(f"Reading logs from {path} with options {options}")
 
-    with open(path, "r", encoding="utf-8", errors="ignore") as file:
+    # By default, universal newline mode is used, which means that all of
+    # \n, \r, or \r\n are recognized as end-of-line characters.
+    # We use os.linesep to ensure that \r is reserved. It's useful for showing progress bars.
+    with open(path, "r", encoding="utf-8", errors="ignore", newline=os.linesep) as file:
         if options.tail > 0:
             # Move to the end of the file and read the last 'tail' lines
             file.seek(0, os.SEEK_END)
