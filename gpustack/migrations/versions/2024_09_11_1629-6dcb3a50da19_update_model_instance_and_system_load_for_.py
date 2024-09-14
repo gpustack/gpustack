c6fdff91dcaeb17bbba483b7aa57f437d4418c1a
michelia <michelia@seal.io>
2024-09-14 18:27:09 +0800

    refactor: update migration script to batch

--- gpustack/migrations/versions/2024_09_11_1629-6dcb3a50da19_update_model_instance_and_system_load_for_.py
+++ gpustack/migrations/versions/2024_09_11_1629-6dcb3a50da19_update_model_instance_and_system_load_for_.py
@@ -23,36 +23,36 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     # system_loads
-    op.alter_column('system_loads', 'memory', new_column_name='ram')
-    op.alter_column('system_loads', 'gpu_memory', new_column_name='vram')
+    with op.batch_alter_table('system_loads') as batch_op:
+        batch_op.alter_column('memory', new_column_name='ram')
+        batch_op.alter_column('gpu_memory', new_column_name='vram')
 
-    # models
-    op.add_column('models', sa.Column('placement_strategy', sa.Enum(
-        'SPREAD', 'BINPACK', name='placementstrategyenum'), nullable=True))
+    with op.batch_alter_table('models') as batch_op:
+        batch_op.add_column(sa.Column('placement_strategy', sa.Enum(
+            'SPREAD', 'BINPACK', name='placementstrategyenum'), nullable=True))
+        batch_op.add_column(sa.Column('cpu_offloading', sa.Boolean(), nullable=True))
+        batch_op.add_column(sa.Column(
+            'distributed_inference_across_workers', sa.Boolean(), nullable=True))
+        batch_op.add_column(sa.Column('worker_selector', sa.JSON(), nullable=True))
+        batch_op.add_column(sa.Column('gpu_selector',
+                                      gpustack.schemas.common.JSON(), nullable=True))
+
     op.execute('UPDATE models SET placement_strategy = "SPREAD"')
+    op.execute('UPDATE models SET cpu_offloading = True')
+    op.execute('UPDATE models SET distributed_inference_across_workers = False')
+
     with op.batch_alter_table('models') as batch_op:
         batch_op.alter_column('placement_strategy', nullable=False)
-
-    op.add_column('models', sa.Column('cpu_offloading', sa.Boolean(), nullable=True))
-    op.execute('UPDATE models SET cpu_offloading = True')
-    with op.batch_alter_table('models') as batch_op:
         batch_op.alter_column('cpu_offloading', nullable=False)
-
-    op.add_column('models', sa.Column(
-        'distributed_inference_across_workers', sa.Boolean(), nullable=True))
-    op.execute('UPDATE models SET distributed_inference_across_workers = False')
-    with op.batch_alter_table('models') as batch_op:
         batch_op.alter_column('distributed_inference_across_workers', nullable=False)
 
-    op.add_column('models', sa.Column('worker_selector', sa.JSON(), nullable=True))
-    op.add_column('models', sa.Column('gpu_selector',
-                  gpustack.schemas.common.JSON(), nullable=True))
-
     # model_instances
-    op.add_column('model_instances', sa.Column('distributed_servers',
-                  gpustack.schemas.common.JSON(), nullable=True))
+    with op.batch_alter_table('model_instances') as batch_op:
+        batch_op.add_column(sa.Column('distributed_servers',
+                                      gpustack.schemas.common.JSON(), nullable=True))
+        batch_op.add_column(sa.Column(
+            'gpu_indexes', sa.JSON(), nullable=True))
 
-    op.add_column('model_instances', sa.Column('gpu_indexes', sa.JSON(), nullable=True))
     conn = op.get_bind()
     model_instances = conn.execute(
         sa.text("SELECT id, gpu_index, computed_resource_claim FROM model_instances")).fetchall()
@@ -74,24 +74,29 @@ def upgrade() -> None:
             {"gpu_indexes": json.dumps(gpu_array), "computed_resource_claim": json.dumps(
                 claim), "id": instance.id}
         )
-    op.drop_column('model_instances', 'gpu_index')
+
+    with op.batch_alter_table('model_instances') as batch_op:
+        batch_op.drop_column('gpu_index')
 
 
 def downgrade() -> None:
     # system_loads
-    op.alter_column('system_loads', 'ram', new_column_name='memory')
-    op.alter_column('system_loads', 'vram', new_column_name='gpu_memory')
+    with op.batch_alter_table('system_loads') as batch_op:
+        batch_op.alter_column('ram', new_column_name='memory')
+        batch_op.alter_column('vram', new_column_name='gpu_memory')
 
     # models
-    op.drop_column('models', 'gpu_selector')
-    op.drop_column('models', 'worker_selector')
-    op.drop_column('models', 'distributed_inference_across_workers')
-    op.drop_column('models', 'cpu_offloading')
-    op.drop_column('models', 'placement_strategy')
+    with op.batch_alter_table('models') as batch_op:
+        batch_op.drop_column('gpu_selector')
+        batch_op.drop_column('worker_selector')
+        batch_op.drop_column('distributed_inference_across_workers')
+        batch_op.drop_column('cpu_offloading')
+        batch_op.drop_column('placement_strategy')
 
     # model_instances
     op.execute('Delete from model_instances')
-    op.drop_column('model_instances', 'distributed_servers')
-    op.drop_column('model_instances', 'gpu_indexes')
-    op.add_column('model_instances', sa.Column(
-        'gpu_index', sa.INTEGER(), nullable=True))
+    with op.batch_alter_table('model_instances') as batch_op:
+        batch_op.drop_column('distributed_servers')
+        batch_op.drop_column('gpu_indexes')
+        batch_op.add_column(sa.Column(
+            'gpu_index', sa.INTEGER(), nullable=True))
