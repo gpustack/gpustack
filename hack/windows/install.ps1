2ecb1d5ca1ff2f7a1dfaa787e7bcdcfbb8006a80
michelia <michelia@seal.io>
2024-09-26 11:10:03 +0800

    chore: update fastfetch to 2.25.0.1

--- hack/windows/install.ps1
+++ hack/windows/install.ps1
@@ -26,7 +26,7 @@ function Install-Dependency {
 }
 
 function Get-FastFetch {
-    $version = "2.18.1.5"
+    $version = "2.25.0.1"
     $fastfetchDir = Join-Path -Path $THIRD_PARTY_DIR -ChildPath "fastfetch"
     $fastfetchTmpDir = Join-Path -Path $fastfetchDir -ChildPath "tmp"
 
