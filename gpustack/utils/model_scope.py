da6279777fca8a90bbd6585101816e3b33cf6bfc
michelia <michelia@seal.io>
2024-09-29 10:50:25 +0800

    fix: subdirectory model file not found for the model scope

--- gpustack/utils/model_scope.py
+++ gpustack/utils/model_scope.py
@@ -4,8 +4,13 @@ from modelscope.hub.api import HubApi
 
 
 def match_model_scope_file_paths(model_id: str, file_path: str) -> List[str]:
+    if '/' in file_path:
+        root, _ = file_path.rsplit('/', 1)
+    else:
+        root = None
+
     api = HubApi()
-    files = api.get_model_files(model_id)
+    files = api.get_model_files(model_id, root=root)
 
     file_paths = [file["Path"] for file in files]
     matching_paths = [p for p in file_paths if fnmatch.fnmatch(p, file_path)]
