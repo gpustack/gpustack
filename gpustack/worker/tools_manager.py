fc0165b4a2fe5f9aa64fa50a23c2970d90a08457
gitlawr <lawrleegle@gmail.com>
2024-10-24 16:23:47 +0800

    chore: bump box

--- gpustack/worker/tools_manager.py
+++ gpustack/worker/tools_manager.py
@@ -73,7 +73,7 @@ class ToolsManager:
         self.download_fastfetch()
 
     def download_llama_box(self):
-        version = "v0.0.67"
+        version = "v0.0.69"
         llama_box_dir = self.bin_path.joinpath("llama-box")
         llama_box_tmp_dir = llama_box_dir.joinpath(llama_box_dir, "tmp")
 
