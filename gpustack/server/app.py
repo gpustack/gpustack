72f3077433e3f0c01a02a20107d93b30164e6f39
gitlawr <lawrleegle@gmail.com>
2024-10-25 20:02:08 +0800

    chore: update version in swagger

--- gpustack/server/app.py
+++ gpustack/server/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi_cdn_host import monkey_patch_for_docs_ui
 import httpx
 
+from gpustack import __version__
 from gpustack.api import exceptions, middlewares
 from gpustack.routes import ui
 from gpustack.routes.routes import api_router
@@ -15,7 +16,12 @@ async def lifespan(app: FastAPI):
     await app.state.http_client.aclose()
 
 
-app = FastAPI(title="GPUStack", lifespan=lifespan, response_model_exclude_unset=True)
+app = FastAPI(
+    title="GPUStack",
+    lifespan=lifespan,
+    response_model_exclude_unset=True,
+    version=__version__,
+)
 monkey_patch_for_docs_ui(app)
 app.add_middleware(middlewares.RequestTimeMiddleware)
 app.add_middleware(middlewares.ModelUsageMiddleware)
