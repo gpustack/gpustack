d9894555786edd066b29a5f51e62ad7635a2f17e
gitlawr <lawrleegle@gmail.com>
2024-11-06 16:57:53 +0800

    fix: parsing multiline chunk

--- gpustack/api/middlewares.py
+++ gpustack/api/middlewares.py
@@ -37,179 +37,174 @@ class ModelUsageMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         if response.status_code == 200:
             if request.url.path == "/v1-openai/chat/completions":
-                return await self.process_request(
-                    request,
-                    response,
-                    self.record_chat_completions_model_usage,
-                    ChatCompletion,
+                return await process_request(
+                    request, response, ChatCompletion, OperationEnum.CHAT_COMPLETION
                 )
             elif request.url.path == "/v1-openai/completions":
-                return await self.process_request(
-                    request, response, self.record_completions_model_usage, Completion
+                return await process_request(
+                    request, response, Completion, OperationEnum.COMPLETION
                 )
             elif request.url.path == "/v1-openai/embeddings":
-                return await self.process_request(
+                return await process_request(
                     request,
                     response,
-                    self.record_embeddings_model_usage,
                     CreateEmbeddingResponse,
+                    OperationEnum.EMBEDDING,
                 )
             elif request.url.path == "/v1/rerank":
-                return await self.process_request(
+                return await process_request(
                     request,
                     response,
-                    self.record_rerank_model_usage,
                     RerankResponse,
+                    OperationEnum.RERANK,
                 )
 
         return response
 
-    async def process_request(
-        self,
-        request: Request,
-        response: StreamingResponse,
-        record_usage,
-        response_class: Type[
-            Union[ChatCompletion, Completion, CreateEmbeddingResponse, RerankResponse]
-        ],
-    ):
-        stream: bool = getattr(request.state, "stream", False)
-        if stream:
-            if response_class == ChatCompletion:
-                response_class = ChatCompletionChunk
-            return await self.handle_streaming_response(
-                request, response, record_usage, response_class
-            )
+
+async def process_request(
+    request: Request,
+    response: StreamingResponse,
+    response_class: Type[
+        Union[ChatCompletion, Completion, CreateEmbeddingResponse, RerankResponse]
+    ],
+    operation: OperationEnum,
+):
+    stream: bool = getattr(request.state, "stream", False)
+    if stream:
+        if response_class == ChatCompletion:
+            response_class = ChatCompletionChunk
+        return await handle_streaming_response(
+            request, response, response_class, operation
+        )
+    else:
+        response_body = b"".join([chunk async for chunk in response.body_iterator])
+        try:
+            response_dict = json.loads(response_body)
+            response_instance = response_class(**response_dict)
+            await record_model_usage(request, response_instance.usage, operation)
+        except Exception as e:
+            logger.error(f"Error processing model usage: {e}")
+        response = Response(content=response_body, headers=dict(response.headers))
+
+    return response
+
+
+async def record_model_usage(
+    request: Request,
+    usage: Union[CompletionUsage, EmbeddingUsage, RerankUsage],
+    operation: OperationEnum,
+):
+    prompt_tokens = usage.prompt_tokens
+    total_tokens = usage.total_tokens
+    completion_tokens = getattr(
+        usage, 'completion_tokens', total_tokens - prompt_tokens
+    )
+    user: User = request.state.user
+    model: Model = request.state.model
+    fields = {
+        "user_id": user.id,
+        "model_id": model.id,
+        "date": date.today(),
+        "operation": operation,
+    }
+    model_usage = ModelUsage(
+        **fields,
+        completion_token_count=completion_tokens,
+        prompt_token_count=prompt_tokens,
+        request_count=1,
+    )
+    async with AsyncSession(get_engine()) as session:
+        current_model_usage = await ModelUsage.one_by_fields(session, fields)
+        if current_model_usage:
+            current_model_usage.completion_token_count += completion_tokens
+            current_model_usage.prompt_token_count += prompt_tokens
+            current_model_usage.request_count += 1
+            await current_model_usage.update(session)
         else:
-            response_body = b"".join([chunk async for chunk in response.body_iterator])
+            await ModelUsage.create(session, model_usage)
+
+
+async def handle_streaming_response(
+    request: Request,
+    response: StreamingResponse,
+    response_class: Type[Union[ChatCompletionChunk, Completion]],
+    operation: OperationEnum,
+):
+    async def streaming_generator():
+        async for chunk in response.body_iterator:
             try:
-                response_dict = json.loads(response_body)
-                response_instance = response_class(**response_dict)
-                await record_usage(request, response_instance.usage)
+                async for processed_chunk in process_chunk(
+                    chunk, request, response_class, operation
+                ):
+                    yield processed_chunk
             except Exception as e:
-                logger.error(f"Error processing model usage: {e}")
-            response = Response(content=response_body, headers=dict(response.headers))
-
-        return response
-
-    async def handle_streaming_response(
-        self,
-        request: Request,
-        response: StreamingResponse,
-        record_usage,
-        response_class: Type[Union[ChatCompletionChunk, Completion]],
-    ):
-        async def streaming_generator():
-            async for chunk in response.body_iterator:
-                try:
-                    if not hasattr(request.state, 'first_token_time'):
-                        request.state.first_token_time = datetime.now(timezone.utc)
-                    data = chunk.decode("utf-8").split('data: ')[-1]
-                    if not data.startswith('[DONE]'):
-                        response_dict = json.loads(data.strip())
-                        response_chunk = response_class(**response_dict)
-                        if is_usage_chunk(response_chunk):
-                            await record_usage(request, response_chunk.usage)
-
-                        # Fill rate metrics. These are extended info not included in OAI APIs.
-                        # llama-box provides them out-of-the-box. Align with other backends here.
-                        # vLLM streams a chunk with empty choices between [DONE] and the one with finish_reason.
-                        if (
-                            is_usage_chunk(response_chunk)
-                            or len(response_chunk.choices) == 0
-                        ):
-                            if 'tokens_per_second' not in response_dict['usage']:
-                                now = datetime.now(timezone.utc)
-                                time_to_first_token_ms = (
-                                    request.state.first_token_time
-                                    - request.state.start_time
-                                ).total_seconds() * 1000
-                                time_per_output_token_ms = (
-                                    (
-                                        now - request.state.first_token_time
-                                    ).total_seconds()
-                                    * 1000
-                                    / max(response_chunk.usage.completion_tokens, 1)
-                                )
-                                tokens_per_second = (
-                                    1000 / time_per_output_token_ms
-                                    if time_per_output_token_ms > 0
-                                    else 0
-                                )
-
-                                response_dict['usage'][
-                                    "time_to_first_token_ms"
-                                ] = time_to_first_token_ms
-                                response_dict['usage'][
-                                    "time_per_output_token_ms"
-                                ] = time_per_output_token_ms
-                                response_dict['usage'][
-                                    "tokens_per_second"
-                                ] = tokens_per_second
-
-                                json_str = json.dumps(
-                                    response_dict, separators=(',', ':')
-                                )
-                                chunk = f"data: {json_str}\n\n".encode("utf-8")
-                except Exception as e:
-                    logger.error(f"Error processing streaming response: {e}")
-
+                logger.error(f"Error processing streaming response: {e}")
                 yield chunk
 
-        return StreamingResponse(streaming_generator(), headers=dict(response.headers))
+    return StreamingResponse(streaming_generator(), headers=dict(response.headers))
 
-    async def record_model_usage(
-        self,
-        request: Request,
-        usage: Union[CompletionUsage, EmbeddingUsage, RerankUsage],
-        operation: OperationEnum,
-    ):
-        prompt_tokens = usage.prompt_tokens
-        total_tokens = usage.total_tokens
-        completion_tokens = getattr(
-            usage, 'completion_tokens', total_tokens - prompt_tokens
+
+async def process_chunk(
+    chunk,
+    request,
+    response_class,
+    operation: OperationEnum,
+):
+    if not hasattr(request.state, 'first_token_time'):
+        request.state.first_token_time = datetime.now(timezone.utc)
+
+    # each chunk may contain multiple data lines
+    lines = chunk.decode("utf-8").split("\n\n")
+    for line in lines[:-1]:
+        data = line.split('data: ')[-1]
+        if data.startswith('[DONE]'):
+            continue
+
+        response_dict = json.loads(data.strip())
+        response_chunk = response_class(**response_dict)
+
+        if is_usage_chunk(response_chunk):
+            await record_model_usage(request, response_chunk.usage, operation)
+
+        # Fill rate metrics. These are extended info not included in OAI APIs.
+        # llama-box provides them out-of-the-box. Align with other backends here.
+        # vLLM streams a chunk with empty choices between [DONE] and the one with finish_reason.
+        if should_add_metrics(response_chunk, response_dict):
+            add_metrics(response_dict, request, response_chunk)
+
+        yield f"data: {json.dumps(response_dict, separators=(',', ':'))}\n\n".encode(
+            "utf-8"
         )
-        user: User = request.state.user
-        model: Model = request.state.model
-        fields = {
-            "user_id": user.id,
-            "model_id": model.id,
-            "date": date.today(),
-            "operation": operation,
+
+
+def should_add_metrics(response_chunk, response_dict):
+    return (
+        is_usage_chunk(response_chunk) or len(response_chunk.choices) == 0
+    ) and 'tokens_per_second' not in response_dict['usage']
+
+
+def add_metrics(response_dict, request, response_chunk):
+    now = datetime.now(timezone.utc)
+    time_to_first_token_ms = (
+        request.state.first_token_time - request.state.start_time
+    ).total_seconds() * 1000
+    time_per_output_token_ms = (
+        (now - request.state.first_token_time).total_seconds()
+        * 1000
+        / max(response_chunk.usage.completion_tokens, 1)
+    )
+    tokens_per_second = (
+        1000 / time_per_output_token_ms if time_per_output_token_ms > 0 else 0
+    )
+
+    response_dict['usage'].update(
+        {
+            "time_to_first_token_ms": time_to_first_token_ms,
+            "time_per_output_token_ms": time_per_output_token_ms,
+            "tokens_per_second": tokens_per_second,
         }
-        model_usage = ModelUsage(
-            **fields,
-            completion_token_count=completion_tokens,
-            prompt_token_count=prompt_tokens,
-            request_count=1,
-        )
-        async with AsyncSession(get_engine()) as session:
-            current_model_usage = await ModelUsage.one_by_fields(session, fields)
-            if current_model_usage:
-                current_model_usage.completion_token_count += completion_tokens
-                current_model_usage.prompt_token_count += prompt_tokens
-                current_model_usage.request_count += 1
-                await current_model_usage.update(session)
-            else:
-                await ModelUsage.create(session, model_usage)
-
-    async def record_chat_completions_model_usage(
-        self, request: Request, usage: CompletionUsage
-    ):
-        await self.record_model_usage(request, usage, OperationEnum.CHAT_COMPLETION)
-
-    async def record_completions_model_usage(
-        self, request: Request, usage: CompletionUsage
-    ):
-        await self.record_model_usage(request, usage, OperationEnum.COMPLETION)
-
-    async def record_embeddings_model_usage(
-        self, request: Request, usage: EmbeddingUsage
-    ):
-        await self.record_model_usage(request, usage, OperationEnum.EMBEDDING)
-
-    async def record_rerank_model_usage(self, request: Request, usage: RerankUsage):
-        await self.record_model_usage(request, usage, OperationEnum.RERANK)
+    )
 
 
 class RefreshTokenMiddleware(BaseHTTPMiddleware):
