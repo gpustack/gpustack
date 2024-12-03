81ac226b9ca02b4c3da03c597d01fd304371c9eb
gitlawr <lawrleegle@gmail.com>
2024-12-03 20:50:18 +0800

    chore: update empty layer

--- gpustack/scheduler/calculator.py
+++ gpustack/scheduler/calculator.py
@@ -82,7 +82,7 @@ def _get_empty_estimate(n_gpu: int = 1) -> estimate:
         uma=0, nonuma=0, handleLayers=None
     )
     memory_estimate = memoryEstimate(
-        offloadLayers=-1,
+        offloadLayers=999,
         fullOffloaded=True,
         ram=empty_layer_memory_estimate,
         vrams=[empty_layer_memory_estimate for _ in range(n_gpu)],
