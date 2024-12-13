5f090a879280599a84a9bc775c82468d85816ce9
gitlawr <lawrleegle@gmail.com>
2024-12-13 19:55:08 +0800

    chore: bump box

--- gpustack/worker/tools_manager.py
+++ gpustack/worker/tools_manager.py
@@ -134,7 +134,7 @@ class ToolsManager:
             )
 
     def download_llama_box(self):
-        version = "v0.0.92"
+        version = "v0.0.93"
         target_dir = self.third_party_bin_path / "llama-box"
         file_name = "llama-box.exe" if self._os == "windows" else "llama-box"
         target_file = target_dir / file_name
