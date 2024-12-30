bf7a97cc1f8d61a4a2db4d9ac0f654d850c3bf0e
gitlawr <lawrleegle@gmail.com>
2024-12-30 11:20:25 +0800

    chore: bump parser

--- gpustack/worker/tools_manager.py
+++ gpustack/worker/tools_manager.py
@@ -362,7 +362,7 @@ class ToolsManager:
         return platform_name
 
     def download_gguf_parser(self):
-        version = "v0.13.5"
+        version = "v0.13.6"
         gguf_parser_dir = self.third_party_bin_path.joinpath("gguf-parser")
         os.makedirs(gguf_parser_dir, exist_ok=True)
 
