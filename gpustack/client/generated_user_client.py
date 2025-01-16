e70bedd34a0ea8e18ceeb5cb7090923dd05a9b87
gitlawr <lawrleegle@gmail.com>
2025-01-16 23:09:36 +0800

    refactor: client async watch

--- gpustack/client/generated_user_client.py
+++ gpustack/client/generated_user_client.py
@@ -1,8 +1,8 @@
+import asyncio
 import json
 from typing import Any, Callable, Dict, Optional
 
 import httpx
-from pydantic import BaseModel
 from gpustack.api.exceptions import raise_if_response_error
 from gpustack.server.bus import Event
 from gpustack.schemas import *
@@ -35,7 +35,7 @@ class UserClient:
             stop_condition = lambda event: False
 
         with self._client.get_httpx_client().stream(
-            "GET", self._url, params=params, timeout=httpx.Timeout(45)
+            "GET", self._url, params=params, timeout=None
         ) as response:
             raise_if_response_error(response)
             for line in response.iter_lines():
@@ -45,6 +45,39 @@ class UserClient:
                     callback(event)
                     if stop_condition(event):
                         break
+
+    async def awatch(
+        self,
+        callback: Callable[[Event], None],
+        stop_condition: Optional[Callable[[Event], bool]] = None,
+        params: Optional[Dict[str, Any]] = None,
+    ):
+        if params is None:
+            params = {}
+        params["watch"] = "true"
+
+        if stop_condition is None:
+            stop_condition = lambda event: False
+
+        async with self._client.get_async_httpx_client().stream(
+            "GET",
+            self._url,
+            params=params,
+            timeout=httpx.Timeout(connect=10, read=None, write=10, pool=10),
+        ) as response:
+            raise_if_response_error(response)
+            lines = response.aiter_lines()
+            while True:
+                try:
+                    line = await asyncio.wait_for(lines.__anext__(), timeout=45)
+                    if line:
+                        event_data = json.loads(line)
+                        event = Event(**event_data)
+                        callback(event)
+                        if stop_condition(event):
+                            break
+                except asyncio.TimeoutError:
+                    raise Exception("watch timeout")
 
     def get(self, id: int) -> UserPublic:
         response = self._client.get_httpx_client().get(f"{self._url}/{id}")
