4b0a79c52874c81c2b5d3d1bace5ad824df46f3e
michelia <michelia@seal.io>
2025-02-10 19:25:25 +0800

    fix: can't manual select dcu

--- gpustack/utils/gpu.py
+++ gpustack/utils/gpu.py
@@ -2,7 +2,9 @@ import re
 
 from typing import Tuple
 
-pattern = r"^(?P<worker_name>.+):(?P<device>cuda|npu|rocm|musa|mps):(?P<gpu_index>\d+)$"
+pattern = (
+    r"^(?P<worker_name>.+):(?P<device>cuda|npu|rocm|musa|mps|dcu):(?P<gpu_index>\d+)$"
+)
 
 
 def parse_gpu_id(input: str) -> Tuple[bool, dict]:
