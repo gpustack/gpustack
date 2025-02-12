db9d611aceeaaad90dc13d867342c6a2ca6b3eff
gitlawr <lawrleegle@gmail.com>
2025-02-12 15:50:59 +0800

    chore: bump dependencies

--- gpustack/worker/tools_manager.py
+++ gpustack/worker/tools_manager.py
@@ -156,7 +156,7 @@ class ToolsManager:
             )
 
     def download_llama_box(self):
-        version = "v0.0.115"
+        version = "v0.0.116"
         target_dir = self.third_party_bin_path / "llama-box"
         file_name = "llama-box.exe" if self._os == "windows" else "llama-box"
         target_file = target_dir / file_name
@@ -396,7 +396,7 @@ class ToolsManager:
         return platform_name
 
     def download_gguf_parser(self):
-        version = "v0.13.15"
+        version = "v0.13.16"
         gguf_parser_dir = self.third_party_bin_path.joinpath("gguf-parser")
         os.makedirs(gguf_parser_dir, exist_ok=True)
 
