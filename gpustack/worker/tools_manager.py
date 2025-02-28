62b845690a5d318ae874cd8dd6a194d1c908c687
michelia <michelia@seal.io>
2025-02-28 10:57:18 +0800

    chore: upgrade gguf-parser to v0.13.18

--- gpustack/worker/tools_manager.py
+++ gpustack/worker/tools_manager.py
@@ -396,7 +396,7 @@ class ToolsManager:
         return platform_name
 
     def download_gguf_parser(self):
-        version = "v0.13.16"
+        version = "v0.13.18"
         gguf_parser_dir = self.third_party_bin_path.joinpath("gguf-parser")
         os.makedirs(gguf_parser_dir, exist_ok=True)
 
