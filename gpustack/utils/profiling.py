d50e2b95f3071de939032c452a61ebcdeb28823a
gitlawr <lawrleegle@gmail.com>
2025-04-02 20:03:04 +0800

    feat: add model info in profiling

--- gpustack/utils/profiling.py
+++ gpustack/utils/profiling.py
@@ -1,4 +1,5 @@
 import asyncio
+import inspect
 import logging
 import time
 
@@ -17,8 +18,9 @@ def time_decorator(func):
             start_time = time.time()
             result = await func(*args, **kwargs)
             end_time = time.time()
+            model_info = get_model_info(func, args, kwargs)
             logger.debug(
-                f"{func.__name__} execution time: {end_time - start_time:.2f} seconds"
+                f"{func.__name__}{model_info} execution time: {end_time - start_time:.2f} seconds"
             )
             return result
 
@@ -35,3 +37,22 @@ def time_decorator(func):
             return result
 
         return sync_wrapper
+
+
+def get_model_info(func, args, kwargs) -> str:
+    """
+    Get model info from the function arguments."""
+    sig = inspect.signature(func)
+    bound_args = sig.bind_partial(*args, **kwargs).arguments
+
+    model = bound_args.get("model")
+    model_name = ""
+    if model and hasattr(model, "name"):
+        model_name = model.name
+
+    if model and hasattr(model, "readable_source"):
+        model_name = model.readable_source
+
+    if model_name:
+        return f"(model: '{model_name}')"
+    return ""
