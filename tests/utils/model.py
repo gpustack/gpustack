0a5facdb40a75b24f000de90178d40abe32212b2
gitlawr <lawrleegle@gmail.com>
2025-04-03 16:01:21 +0800

    fix: update non-LLM vllm claim

--- tests/utils/model.py
+++ tests/utils/model.py
@@ -1,4 +1,5 @@
 from gpustack.schemas.models import (
+    CategoryEnum,
     Model,
     ModelInstance,
     ModelInstanceStateEnum,
@@ -45,6 +46,7 @@ def new_model(
     )
     gpu_selector = kargs.get("gpu_selector", None)
     backend_parameters = kargs.get("backend_parameters")
+    categories = kargs.get("categories", [CategoryEnum.LLM])
 
     source = SourceEnum.OLLAMA_LIBRARY
     huggingface_filename = None
@@ -68,4 +70,5 @@ def new_model(
         distributed_inference_across_workers=distributed_inference_across_workers,
         gpu_selector=gpu_selector,
         backend_parameters=backend_parameters,
+        categories=categories,
     )
