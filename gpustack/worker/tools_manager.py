bc5632f856d8cef83e317630a2f21480938a5c73
gitlawr <lawrleegle@gmail.com>
2025-05-16 22:07:43 +0800

    chore: update llama-box to 0.0.144

--- gpustack/worker/tools_manager.py
+++ gpustack/worker/tools_manager.py
@@ -22,7 +22,7 @@ from gpustack.config.config import get_global_config
 logger = logging.getLogger(__name__)
 
 
-BUILTIN_LLAMA_BOX_VERSION = "v0.0.143"
+BUILTIN_LLAMA_BOX_VERSION = "v0.0.144"
 BUILTIN_GGUF_PARSER_VERSION = "v0.17.5"
 BUILTIN_RAY_VERSION = "2.43.0"
 
