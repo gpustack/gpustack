d712623f52d20b1bc88f4bb5cfd0e3d19522644f
michelia <michelia@seal.io>
2025-10-30 20:02:20 +0800

    fix: greenlet issue triggered by incorrect SQLAlchemy async session handling

--- gpustack/server/controllers.py
+++ gpustack/server/controllers.py
@@ -369,17 +369,22 @@ async def ensure_instance_model_file(session: AsyncSession, instance: ModelInsta
         await sync_instance_files_state(session, instance, instance.model_files)
         return
 
+    retry_model_files = []
     model_files = await get_or_create_model_files_for_instance(session, instance)
     for model_file in model_files:
         if model_file.state == ModelFileStateEnum.ERROR:
             # Retry the download
+            retry_model_files.append(model_file.readable_source)
+
             model_file.state = ModelFileStateEnum.DOWNLOADING
             model_file.download_progress = 0
             model_file.state_message = ""
-            await model_file.update(session)
+            await model_file.update(session, auto_commit=False)
 
+    if retry_model_files:
+        await session.commit()
         logger.info(
-            f"Retrying download for model file {model_file.readable_source} for model instance {instance.name}"
+            f"Retrying download for model files {retry_model_files} for model instance {instance.name}"
         )
 
     instance = await ModelInstance.one_by_id(session, instance.id)
