132797fdf299df74f333c81ec4b3b3ed53cec49d
thxCode <thxcode0824@gmail.com>
2025-11-04 09:02:27 +0800

    chore: adjust pack
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- hack/package.sh
+++ hack/package.sh
@@ -9,9 +9,10 @@ source "${ROOT_DIR}/hack/lib/init.sh"
 
 PACKAGE_NAMESPACE=${PACKAGE_NAMESPACE:-gpustack}
 PACKAGE_REPOSITORY=${PACKAGE_REPOSITORY:-gpustack}
-PACKAGE_OS=${PACKAGE_OS:-$(uname -s | tr '[:upper:]' '[:lower:]')}
 PACKAGE_ARCH=${PACKAGE_ARCH:-$(uname -m | sed 's/aarch64/arm64/' | sed 's/x86_64/amd64/')}
 PACKAGE_TAG=${PACKAGE_TAG:-dev}
+PACKAGE_WITH_CACHE=${PACKAGE_WITH_CACHE:-true}
+PACKAGE_PUSH=${PACKAGE_PUSH:-false}
 
 function pack() {
     if ! command -v docker &>/dev/null; then
@@ -22,31 +23,39 @@ function pack() {
     if ! docker buildx inspect --builder "gpustack" &>/dev/null; then
         gpustack::log::info "Creating new buildx builder 'gpustack'"
         docker run --rm --privileged tonistiigi/binfmt:qemu-v9.2.2-52 --uninstall qemu-*
-        docker run --rm --privileged tonistiigi/binfmt:qemu-v9.2.2 --install all
+        docker run --rm --privileged tonistiigi/binfmt:qemu-v9.2.2-52 --install all
         docker buildx create \
             --name "gpustack" \
             --driver "docker-container" \
-            --buildkitd-flags "--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host" \
             --driver-opt "network=host,default-load=true,env.BUILDKIT_STEP_LOG_MAX_SIZE=-1,env.BUILDKIT_STEP_LOG_MAX_SPEED=-1" \
+            --buildkitd-flags "--allow-insecure-entitlement=security.insecure --allow-insecure-entitlement=network.host --oci-worker-net=host --oci-worker-gc-keepstorage=204800" \
             --bootstrap
     fi
 
     TAG="${PACKAGE_NAMESPACE}/${PACKAGE_REPOSITORY}:${PACKAGE_TAG}"
-    gpustack::log::info "Building '${TAG}' platform '${PACKAGE_OS}/${PACKAGE_ARCH}'"
+    EXTRA_ARGS=()
+	if [[ "${PACKAGE_WITH_CACHE}" == "true" ]]; then
+		EXTRA_ARGS+=("--cache-from=type=registry,ref=gpustack/build-cache:gpustack-main")
+	fi
+	if [[ "${PACKAGE_PUSH}" == "true" ]]; then
+		EXTRA_ARGS+=("--push")
+	fi
+    gpustack::log::info "Building '${TAG}' platform 'linux/${PACKAGE_ARCH}'"
+    set -x
     docker buildx build \
         --pull \
         --allow network.host \
         --allow security.insecure \
         --builder "gpustack" \
-        --platform "${PACKAGE_OS}/${PACKAGE_ARCH}" \
+        --platform "linux/${PACKAGE_ARCH}" \
         --tag "${TAG}" \
         --file "${ROOT_DIR}/pack/Dockerfile" \
-        --attest "type=provenance,disabled=true" \
-        --attest "type=sbom,disabled=true" \
         --ulimit nofile=65536:65536 \
+        --shm-size 16G \
         --progress plain \
-        --cache-from "type=registry,ref=gpustack/build-cache:gpustack-main" \
+        "${EXTRA_ARGS[@]}" \
         "${ROOT_DIR}"
+    set +x
 }
 
 gpustack::log::info "+++ PACKAGE +++"
