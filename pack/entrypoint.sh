f3f081100249ac6c9f782341905590b033c26ca1
Yuxing Deng <jxfa0043379@hotmail.com>
2025-11-06 17:45:02 +0800

    fix: prevent higress start too early by force re-generate config

--- pack/entrypoint.sh
+++ pack/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# shellcheck disable=SC1091,SC1090
+
 set -e
 
+source /etc/s6-overlay/scripts/base.sh
 ARGS_FILE="/var/lib/gpustack/run/args/gpustack"
 mkdir -p "$(dirname "$ARGS_FILE")"
 
@@ -15,5 +18,8 @@ else
     echo "[INFO] No docker run args passed."
     : > "$ARGS_FILE"
 fi
+
+# remove generated gateway config to force regeneration
+rm -rf "${GPUSTACK_GATEWAY_CONFIG}"
 
 exec /init
