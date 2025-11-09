7b61ff196624682f555032ab1ff13478e7bfd3a3
thxCode <thxcode0824@gmail.com>
2025-11-09 22:02:40 +0800

    feat: cmd for images
    - gpustack list-images to list available images, include runtime and runner.
    - gpustack save-images to save available images to local path as Docker Archive.
    - gpustack copy-images to copy available images to destination registry.
    
    Signed-off-by: thxCode <thxcode0824@gmail.com>

---
+++ gpustack/cmd/images.py
@@ -0,0 +1,19 @@
+import argparse
+
+from gpustack import __version__
+
+from gpustack_runtime.cmds import (
+    CopyImagesSubCommand,
+    ListImagesSubCommand,
+    SaveImagesSubCommand,
+    append_images,
+)
+
+# Append images used by GPUStack here.
+append_images(f"gpustack/gpustack:{'main' if __version__ == '0.0.0' else __version__}")
+
+
+def setup_images_cmd(subparsers: argparse._SubParsersAction):
+    ListImagesSubCommand.register(subparsers)
+    SaveImagesSubCommand.register(subparsers)
+    CopyImagesSubCommand.register(subparsers)
