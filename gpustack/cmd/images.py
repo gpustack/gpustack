19570f15ae57fbc977921a056a3a08bff58daa6c
thxCode <thxcode0824@gmail.com>
2025-11-14 10:28:08 +0800

    fix: incorrect developing image tag
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- gpustack/cmd/images.py
+++ gpustack/cmd/images.py
@@ -10,7 +10,9 @@ from gpustack_runtime.cmds import (
 )
 
 # Append images used by GPUStack here.
-append_images(f"gpustack/gpustack:{'main' if __version__ == '0.0.0' else __version__}")
+append_images(
+    f"gpustack/gpustack:{'main' if __version__.removeprefix('v') == '0.0.0' else __version__}",
+)
 
 
 def setup_images_cmd(subparsers: argparse._SubParsersAction):
