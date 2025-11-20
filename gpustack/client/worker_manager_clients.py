d130399b3aeb1e09ddf2d018a484453e708b4659
gitlawr <lawrleegle@gmail.com>
2025-11-20 21:32:06 +0800

    fix: api path in registration client

--- gpustack/client/worker_manager_clients.py
+++ gpustack/client/worker_manager_clients.py
@@ -10,7 +10,7 @@ from .generated_http_client import HTTPClient
 class WorkerStatusClient:
     def __init__(self, client: HTTPClient):
         self._client = client
-        self._url = f"{client._base_url}/v1/worker-status"
+        self._url = f"{client._base_url}/v2/worker-status"
 
     def create(self, model_create: WorkerStatusPublic):
         response = self._client.get_httpx_client().post(
@@ -25,7 +25,7 @@ class WorkerStatusClient:
 class WorkerRegistrationClient:
     def __init__(self, client: HTTPClient):
         self._client = client
-        self._url = f"{client._base_url}/v1/workers"
+        self._url = f"{client._base_url}/v2/workers"
 
     def create(self, model_create: WorkerCreate):
         response = self._client.get_httpx_client().post(
