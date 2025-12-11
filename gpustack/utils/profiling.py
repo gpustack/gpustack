dd2a379a4d434487e3c00b1b112d6e93bfc926b1
michelia <michelia@seal.io>
2025-12-11 16:11:24 +0800

    fix: time_decorator error

--- gpustack/utils/profiling.py
+++ gpustack/utils/profiling.py
@@ -7,31 +7,32 @@ import time
 logger = logging.getLogger(__name__)
 
 
-def time_decorator(log_slow_seconds: float = None):
+def time_decorator(func=None, *, log_slow_seconds: float = None):
     """A decorator that logs the execution time of a function.
 
     Args:
+        func: The function to be decorated.
         log_slow_seconds (float, optional): Threshold in seconds to log slow executions. None means log all executions.
     """
 
-    def decorator(func):
-        if asyncio.iscoroutinefunction(func):
+    def decorator(inner_func):
+        if asyncio.iscoroutinefunction(inner_func):
 
             async def async_wrapper(*args, **kwargs):
                 start_time = time.time()
-                result = await func(*args, **kwargs)
+                result = await inner_func(*args, **kwargs)
                 end_time = time.time()
-                model_info = get_model_info(func, args, kwargs)
+                model_info = get_model_info(inner_func, args, kwargs)
 
                 if log_slow_seconds:
                     # Only log if execution time exceeds threshold
                     if (end_time - start_time) > log_slow_seconds:
                         logger.debug(
-                            f"{func.__name__}{model_info} execution time: {end_time - start_time:.2f} seconds, exceeded threshold of {log_slow_seconds} seconds"
+                            f"{inner_func.__name__}{model_info} execution time: {end_time - start_time:.2f} seconds, exceeded threshold of {log_slow_seconds} seconds"
                         )
                 else:
                     logger.debug(
-                        f"{func.__name__}{model_info} execution time: {end_time - start_time:.2f} seconds"
+                        f"{inner_func.__name__}{model_info} execution time: {end_time - start_time:.2f} seconds"
                     )
                 return result
 
@@ -40,23 +41,26 @@ def time_decorator(log_slow_seconds: float = None):
 
             def sync_wrapper(*args, **kwargs):
                 start_time = time.time()
-                result = func(*args, **kwargs)
+                result = inner_func(*args, **kwargs)
                 end_time = time.time()
                 if log_slow_seconds:
                     # Only log if execution time exceeds threshold
                     if (end_time - start_time) > log_slow_seconds:
                         logger.debug(
-                            f"{func.__name__} execution time: {end_time - start_time} seconds, exceeded threshold of {log_slow_seconds} seconds"
+                            f"{inner_func.__name__} execution time: {end_time - start_time} seconds, exceeded threshold of {log_slow_seconds} seconds"
                         )
                 else:
                     logger.debug(
-                        f"{func.__name__} execution time: {end_time - start_time} seconds"
+                        f"{inner_func.__name__} execution time: {end_time - start_time} seconds"
                     )
                 return result
 
             return sync_wrapper
 
-    return decorator
+    if func is None:
+        return decorator
+    else:
+        return decorator(func)
 
 
 def get_model_info(func, args, kwargs) -> str:
