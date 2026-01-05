accebf2b09157e9a00dd1dfda34f220f93740440
michelia <michelia@seal.io>
2026-01-05 15:39:31 +0800

    refactor: merge migration script

--- gpustack/migrations/versions/2026_01_05_1048-53667f33f000_v2_1_0_database_changes.py
+++ gpustack/migrations/versions/2026_01_05_1048-53667f33f000_v2_1_0_database_changes.py
@@ -1,4 +1,4 @@
-"""add api_detected_backend_version to model_instance
+"""v2.1.0 database changes
 
 Revision ID: 53667f33f000
 Revises: 2aed534bd7b2
@@ -32,7 +32,8 @@ cluster_state_to_add = ['PENDING']
 def upgrade() -> None:
     with op.batch_alter_table('model_instances', schema=None) as batch_op:
         batch_op.add_column(sa.Column('api_detected_backend_version', sqlmodel.sql.sqltypes.AutoString(), nullable=True))
-    
+        batch_op.add_column(sa.Column('gpu_type', sqlmodel.sql.sqltypes.AutoString(), nullable=True))
+        
     sql_enum.add_enum_values(
         {'clusters': 'state'},
         cluster_state_enum,
@@ -43,6 +44,7 @@ def upgrade() -> None:
 def downgrade() -> None:
     with op.batch_alter_table('model_instances', schema=None) as batch_op:
         batch_op.drop_column('api_detected_backend_version')
+        batch_op.drop_column('gpu_type')
 
     sql_enum.remove_enum_values(
         {'clusters': ('state', 'PROVISIONING')},
