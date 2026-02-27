3e84eefb9ce9d5172ac24b671bde40f7b665fe55
gitlawr <lawrleegle@gmail.com>
2026-02-27 12:40:05 +0800

    chore: update the default db pool size

--- gpustack/envs/__init__.py
+++ gpustack/envs/__init__.py
@@ -4,8 +4,8 @@ import os
 
 # Database configuration
 DB_ECHO = os.getenv("GPUSTACK_DB_ECHO", "false").lower() == "true"
-DB_POOL_SIZE = int(os.getenv("GPUSTACK_DB_POOL_SIZE", 10))
-DB_MAX_OVERFLOW = int(os.getenv("GPUSTACK_DB_MAX_OVERFLOW", 10))
+DB_POOL_SIZE = int(os.getenv("GPUSTACK_DB_POOL_SIZE", 30))
+DB_MAX_OVERFLOW = int(os.getenv("GPUSTACK_DB_MAX_OVERFLOW", 20))
 DB_POOL_TIMEOUT = int(os.getenv("GPUSTACK_DB_POOL_TIMEOUT", 30))
 
 # Proxy configuration
