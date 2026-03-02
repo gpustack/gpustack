f07ededbae1e3a432148f5be20857f57db902605
yxf <xf.ye@gpustack.ai>
2026-03-02 15:48:24 +0800

    feat: Load the list of models supported by each backend using YAML

--- gpustack/worker/backends/sglang.py
+++ gpustack/worker/backends/sglang.py
@@ -23,6 +23,7 @@ from gpustack.schemas.models import (
     SpeculativeAlgorithmEnum,
     CategoryEnum,
     ModelInstanceDeploymentMetadata,
+    BackendEnum,
 )
 from gpustack.utils.command import (
     find_bool_parameter,
@@ -375,7 +376,9 @@ class SGLangServer(InferenceServer):
         arguments.extend(cache_report_arguments)
 
         # Add multimodal argument if needed
-        if is_multimodal_model(self._get_model_architecture()):
+        if is_multimodal_model(
+            self._get_model_architecture(), backend=BackendEnum.SGLANG
+        ):
             arguments.append("--enable-multimodal")
 
         # Add speculative config arguments if needed
@@ -419,7 +422,9 @@ class SGLangServer(InferenceServer):
                     "ascend",
                 ]
             )
-            if is_multimodal_model(self._get_model_architecture()):
+            if is_multimodal_model(
+                self._get_model_architecture(), backend=BackendEnum.SGLANG
+            ):
                 arguments.extend(
                     [
                         "--mm-attention-backend",
