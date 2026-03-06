b5e2bac49df34825a5ad470698c09223f854a8e0
Yuxing Deng <jxfa0043379@hotmail.com>
2026-03-06 12:07:07 +0800

    fix: construct JSON response for connection error

--- gpustack/api/auth.py
+++ gpustack/api/auth.py
@@ -46,6 +46,13 @@ credentials_exception = UnauthorizedException(
 )
 
 
+def client_ip_getter(request: Request) -> str:
+    if request.app.state.server_config.gateway_mode == GatewayModeEnum.embedded:
+        return request.headers.get("X-GPUStack-Real-IP", "")
+    else:
+        return request.client.host
+
+
 async def get_current_user(
     request: Request,
     session: Annotated[AsyncSession, Depends(get_session)],
@@ -62,38 +69,33 @@ async def get_current_user(
         return user
     api_key: Optional[ApiKey] = None
     user = None
-    if basic_credentials and is_system_user(basic_credentials.username):
+    try:
         server_config: Config = request.app.state.server_config
-        user = await authenticate_system_user(server_config, basic_credentials)
-    elif basic_credentials:
-        user = await authenticate_basic_user(session, basic_credentials)
-    elif cookie_token:
-        jwt_manager: JWTManager = request.app.state.jwt_manager
-        user = await get_user_from_jwt_token(session, jwt_manager, cookie_token)
-    elif bearer_token:
-        user, api_key = await get_user_from_bearer_token(session, bearer_token)
+        if basic_credentials and is_system_user(basic_credentials.username):
+            user = await authenticate_system_user(server_config, basic_credentials)
+        elif basic_credentials:
+            user = await authenticate_basic_user(session, basic_credentials)
+        elif cookie_token:
+            jwt_manager: JWTManager = request.app.state.jwt_manager
+            user = await get_user_from_jwt_token(session, jwt_manager, cookie_token)
+        elif bearer_token:
+            user, api_key = await get_user_from_bearer_token(session, bearer_token)
 
-    server_config: Config = request.app.state.server_config
-
-    def client_ip_getter() -> str:
-        if server_config.gateway_mode == GatewayModeEnum.embedded:
-            return request.headers.get("X-GPUStack-Real-IP", "")
-        else:
-            return request.client.host
-
-    if user is None and client_ip_getter() == "127.0.0.1":
-        if not server_config.force_auth_localhost:
-            try:
+        if user is None and client_ip_getter(request=request) == "127.0.0.1":
+            if not server_config.force_auth_localhost:
                 user = await User.first_by_field(session, "is_admin", True)
-            except Exception as e:
-                raise InternalServerErrorException(message=f"Failed to get user: {e}")
-    if user:
-        if not user.is_active:
-            raise UnauthorizedException(message="User account is deactivated")
-        request.state.user = user
-        if api_key is not None:
-            request.state.api_key = api_key
-        return user
+        if user:
+            if not user.is_active:
+                raise UnauthorizedException(message="User account is deactivated")
+            request.state.user = user
+            if api_key is not None:
+                request.state.api_key = api_key
+            return user
+
+    except UnauthorizedException:
+        raise
+    except Exception as e:
+        raise InternalServerErrorException(message=f"Failed to authenticate user: {e}")
 
     raise credentials_exception
 
