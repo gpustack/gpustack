72eee0680c28b9e2347b487fdc23acb6dba9c84b
foyou <yimi.0822@qq.com>
2026-03-27 14:15:14 +0800

    docs: refactor tablesort initialization for improved readability

--- docs/javascripts/tablesort.js
+++ docs/javascripts/tablesort.js
@@ -1,6 +1,6 @@
-document$.subscribe(function () {
-    var tables = document.querySelectorAll("article table:not([class])")
-    tables.forEach(function (table) {
-        new Tablesort(table)
+document$.subscribe(() => {
+    const tables = document.querySelectorAll("article table:not([class])");
+    tables.forEach(table => {
+        new Tablesort(table);
     });
 });
