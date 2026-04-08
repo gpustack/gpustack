cc63889a03fb29e2e22d3b80fb37be18cd50b3ef
Yuxing Deng <jxfa0043379@hotmail.com>
2026-04-08 10:28:07 +0800

    fix: merge k8s volume mount migration script

--- gpustack/migrations/versions/2026_03_10_1600-8bf38a6bb3b5_v2_2_0_database_changes.py
+++ gpustack/migrations/versions/2026_03_10_1600-8bf38a6bb3b5_v2_2_0_database_changes.py
@@ -9,6 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+import gpustack
 
 # revision identifiers, used by Alembic.
 revision: str = '8bf38a6bb3b5'
@@ -20,8 +21,18 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     with op.batch_alter_table('workers', schema=None) as batch_op:
         batch_op.add_column(sa.Column('worker_version', sa.String(100), nullable=True))
+    
+    ### k8s volume mount
+    with op.batch_alter_table('clusters', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('k8s_volume_mounts', gpustack.schemas.common.JSON(), nullable=True))
+    ### end
 
 
 def downgrade() -> None:
     with op.batch_alter_table('workers', schema=None) as batch_op:
         batch_op.drop_column('worker_version')
+
+    ### k8s volume mount
+    with op.batch_alter_table('clusters', schema=None) as batch_op:
+        batch_op.drop_column('k8s_volume_mounts')
+    ### end
