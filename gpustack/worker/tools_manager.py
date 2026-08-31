52a5b9192545a6ba1945a503fd796b2fb16bc6f9
thxCode <thxcode0824@gmail.com>
2026-08-31 12:14:44 +0800

    chore(tool): bump gguf-parser version
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- gpustack/worker/tools_manager.py
+++ gpustack/worker/tools_manager.py
@@ -16,7 +16,7 @@ from gpustack.worker.backend_dependency_manager import BackendDependencyManager
 logger = logging.getLogger(__name__)
 
 
-BUILTIN_GGUF_PARSER_VERSION = "v0.26.0"
+BUILTIN_GGUF_PARSER_VERSION = "v0.26.2"
 
 
 class ToolsManager:
